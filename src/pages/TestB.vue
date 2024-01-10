96bc23f262437a88e8cf51a03b50bd036ea1848f
zwl <990708269@qq.com>
2024-01-10 16:05:08 +0800

    使用 Vue Language Features (volar)的哨兵模式，详见readme文档介绍

--- src/pages/TestB.vue
+++ src/pages/TestB.vue
@@ -3,10 +3,11 @@
 </template>
 
 <script setup lang="ts">
-  import { inject } from 'vue';
+  import { inject,provide } from 'vue';
   import { showPopupKey, titleKey } from '@/keys';
 
   const key = inject(titleKey, '20');
+
 
   // 如果不设置默认值，也不使用InjectionKey<T>类型的key,那么key1的类型为unknown
   let key1 = inject('test', 20);
